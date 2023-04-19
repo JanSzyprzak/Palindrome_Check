150ff459c4767e2e42f7c900b9cd8904ca13de37
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-19 11:41:48 +0200

    Add test cases

--- palindrome_check.py
+++ palindrome_check.py
@@ -1,14 +1,17 @@
 
 
 
-pal = "Ile roman ładny dyndał na moreli."
 
 def is_palindrome(text):
     
     alnum_signs = [x for x in text.lower() if x.isalnum()]
     if alnum_signs == alnum_signs[::-1]:
-        return "To jest palindrom."
+        return f"'{word}' jest palindromem"
     else:
-        return "To nie jest palindrom"
+        return f"'{word}' nie jest palindromem"
 
-print(is_palindrome(pal))
+
+test_cases = ['sos', 'soso', 'Sos', 'Soso', '', '111', 's,o,s!!!!!!', "Ile roman ładny dyndał na moreli."]
+
+for word in test_cases:
+    print(is_palindrome(word))
